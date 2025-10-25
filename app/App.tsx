4607ae9f1c64c4386a9ccaad4c915831d5141604
KhaiTien <khaitien600@gmail.com>
2025-10-25 07:38:55 +0700

    Trang Home Demo

---
+++ app/App.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from './HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,5 @@
+// index.tsx
+import { registerRootComponent } from "expo";
+import App from "./App";
+
+registerRootComponent(App);
